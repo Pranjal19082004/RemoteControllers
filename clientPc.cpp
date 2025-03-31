6c83a979ed352333e7add713d80f03c6283960eb
Pranjal <pranjal.indhanpay@gmail.com>
2025-03-31 23:49:49 +0530

    added keyboard utility in the program

--- clientPc.cpp
+++ clientPc.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
+#undef _WIN32_WINNT
+#define _WIN32_WINNT 0x0601
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include "mouseEvents.h"
@@ -9,7 +12,7 @@
 
 using namespace std;
 SOCKET find_server(char *NODENAME);
-void HandleEvent(int eventNumber, int16_t x, int16_t y);
+void HandleEventForMouse(int eventNumber, int16_t x, int16_t y, int16_t width, int16_t height, int16_t clientWidth, int16_t clientHeight);
 
 int main(int argc, char *argv[])
 {
@@ -25,11 +28,12 @@ int main(int argc, char *argv[])
 	}
 	cout << "Connected " << "\n";
 	char *messageBuffer = new char[10];
+	cout << sizeof(WPARAM) << endl;
 
 	while (true)
 	{
 		long long n = 0;
-		n = recv(cfd, messageBuffer, 5, 0);
+		n = recv(cfd, messageBuffer, 10, 0);
 		if (n == 0)
 		{
 			cout << "done" << endl;
@@ -37,17 +41,61 @@ int main(int argc, char *argv[])
 		}
 
 		int eventType = static_cast<int>(messageBuffer[0]);
+		if (eventType == 8)
+		{
+			INPUT input = {0};
+			UINT16 wvk1 = static_cast<unsigned char>(messageBuffer[1]);
+			UINT8 wvk2 = static_cast<unsigned char>(messageBuffer[2]);
+			UINT16 wvk = ((wvk1) | (wvk2 << 8));
+			UINT8 flag = static_cast<unsigned char>(messageBuffer[3]);
+			if (flag == 1)
+			{
+				input.type = INPUT_KEYBOARD;
+				input.ki.wVk = wvk;
+				input.ki.wScan = MapVirtualKey(wvk, MAPVK_VK_TO_VSC);
+				input.ki.dwFlags = KEYEVENTF_SCANCODE;
+				if (wvk >= VK_LEFT && wvk <= VK_DOWN)
+				{ // Handle extended keys
+					input.ki.dwFlags |= KEYEVENTF_EXTENDEDKEY;
+				}
+				input.ki.dwFlags |= KEYEVENTF_KEYUP;
+				// Press key
+				SendInput(1, &input, sizeof(INPUT));
+				continue;
+			}
+			iput.type = INPUT_KEYBOARD;
+			input.ki.wVk = wvk;
+			inut.ki.wScan = MapVirtualKey(wvk, MAPVK_VK_TO_VSC);
+			input.ki.dwFlags = KEYEVENTF_SCANCODE;
+
+			if (wvk >= VK_LEFT && wvk <= VK_DOWN)
+			{ // Handle extended keys
+				input.ki.dwFlags |= KEYEVENTF_EXTENDEDKEY;
+			}
+			SendInput(1, &input, sizeof(INPUT));
+
+			if (flag == 0)
+			{
+				cout << (char)(wvk) << endl;
+			}
+			continue;
+		}
 		int16_t x1 = (static_cast<unsigned char>(messageBuffer[1]));
 		int16_t x2 = (static_cast<unsigned char>(messageBuffer[2]));
 		int16_t x = (x1 | (x2 << 8));
-
 		int16_t y1 = (static_cast<unsigned char>(messageBuffer[3]));
 		int16_t y2 = static_cast<unsigned char>(messageBuffer[4]);
 		int16_t y = (y1 | (y2 << 8));
+		int16_t width1 = static_cast<unsigned char>(messageBuffer[5]);
+		int16_t width2 = static_cast<unsigned char>(messageBuffer[6]);
+		int16_t clientWidth = (width1 | (width2 << 8));
+		int16_t height1 = static_cast<unsigned char>(messageBuffer[7]);
+		int16_t height2 = static_cast<unsigned char>(messageBuffer[8]);
+		int16_t clientHeight = (height1 | (height2 << 8));
 		cout << x << " " << y << endl;
-		// cout<< messageBuffer[1] |
-
-		HandleEvent(eventType, x, y);
+		int width = GetSystemMetrics(SM_CXSCREEN);
+		int height = GetSystemMetrics(SM_CYSCREEN);
+		HandleEventForMouse(eventType, x, y, width, height, clientWidth, clientHeight);
 	}
 	closesocket(cfd);
 	WSACleanup();
@@ -88,9 +136,15 @@ SOCKET find_server(char *NODENAME)
 	}
 	return clientFd;
 }
-void HandleEvent(int eventNumber, int16_t x, int16_t y)
+void HandleEventForMouse(int eventNumber, int16_t x, int16_t y, int16_t width, int16_t height, int16_t clientWidth, int16_t clientHeight)
 {
 
+	unsigned int xtemp = (x * width);
+	xtemp = (unsigned int)(((double)(xtemp)) / clientWidth);
+	int ytemp = (y * height);
+	ytemp = (unsigned int)(((double)(ytemp)) / (clientHeight));
+	x = static_cast<unsigned int16_t>(xtemp);
+	y = static_cast<unsigned int16_t>(ytemp);
 	switch (eventNumber)
 	{
 	case 1:
@@ -109,7 +163,7 @@ void HandleEvent(int eventNumber, int16_t x, int16_t y)
 		cout << "left button double click" << endl;
 		break;
 	case 5:
-		holdLeft(x, y);
+		holdRight(x, y);
 		cout << "right button down " << endl;
 		break;
 	case 6:
@@ -119,6 +173,9 @@ void HandleEvent(int eventNumber, int16_t x, int16_t y)
 	case 7:
 		rightDoubleClick(x, y);
 		cout << "right button double clicked" << endl;
+		break;
+	case 8:
+		cout << "keyboard is pressed ";
 		break;
 	default:
 		break;
--- serverPc.cpp
+++ serverPc.cpp
@@ -12,160 +12,201 @@
 using namespace std;
 SOCKET nfd;
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
+void LogMessage(const std::string &message);
+void sendKeyBoardEvents(HWND &hwnd, UINT &uMsg, WPARAM &wParam, LPARAM &lParam);
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
 
-    const wchar_t CLASS_NAME[] = L"Sample Window Class";
+	const wchar_t CLASS_NAME[] = L"Sample Window Class";
 
-    WNDCLASS wc = {};
+	WNDCLASS wc = {};
 
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
 
-    RegisterClass(&wc);
+	RegisterClass(&wc);
 
-    // Create the window.
+	// Create the window.
 
-    HWND hwnd = CreateWindowEx(
-        0,
-        CLASS_NAME,
-        L"Learn to Program Windows",
-        WS_OVERLAPPEDWINDOW,
+	HWND hwnd = CreateWindowEx(
+		0,
+		CLASS_NAME,
+		L"Learn to Program Windows",
+		WS_OVERLAPPEDWINDOW,
 
-        // Size and position
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		// Size and position
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
 
-        NULL,      // Parent window
-        NULL,      // Menu
-        hInstance, // Instance handle
-        NULL       // Additional application data
-    );
+		NULL,	   // Parent window
+		NULL,	   // Menu
+		hInstance, // Instance handle
+		NULL	   // Additional application data
+	);
 
-    if (hwnd == NULL)
-    {
-        MessageBox(NULL, L"Failed to create window!", L"Error", MB_OK | MB_ICONERROR);
-        return 0;
-    }
-    RECT rect;
-    GetWindowRect(hwnd, &rect);
-    ShowWindow(hwnd, nCmdShow);
-    WSADATA d;
-    if (WSAStartup(MAKEWORD(2, 2), &d) != 0)
-    {
-        MessageBox(NULL, L"WSAstartup", L"Error", MB_OK | MB_ICONERROR);
-        return 1;
-    }
-    SOCKET sfd;
-    struct sockaddr_storage theirAddr;
-    int addrLen = INET6_ADDRSTRLEN;
-    if ((sfd = find_a_listener()) == INVALID_SOCKET)
-    {
-        MessageBox(NULL, L"find_a_listener", L"Error", MB_OK | MB_ICONERROR);
-        return 1;
-    };
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, L"Failed to create window!", L"Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+	RECT rect;
+	GetWindowRect(hwnd, &rect);
+	ShowWindow(hwnd, nCmdShow);
+	WSADATA d;
+	if (WSAStartup(MAKEWORD(2, 2), &d) != 0)
+	{
+		MessageBox(NULL, L"WSAstartup", L"Error", MB_OK | MB_ICONERROR);
+		return 1;
+	}
+	SOCKET sfd;
+	struct sockaddr_storage theirAddr;
+	int addrLen = INET6_ADDRSTRLEN;
+	if ((sfd = find_a_listener()) == INVALID_SOCKET)
+	{
+		MessageBox(NULL, L"find_a_listener", L"Error", MB_OK | MB_ICONERROR);
+		return 1;
+	};
 
-    if ((nfd = accept(sfd, (sockaddr *)&theirAddr, &addrLen)) == INVALID_SOCKET)
-    {
-        MessageBox(NULL, L"accept", L"Error", MB_OK | MB_ICONERROR);
-        closesocket(sfd);
-        PostQuitMessage(0);
-        WSACleanup();
-        return 0;
-    }
-    // Run the message loop.
+	if ((nfd = accept(sfd, (sockaddr *)&theirAddr, &addrLen)) == INVALID_SOCKET)
+	{
+		MessageBox(NULL, L"accept", L"Error", MB_OK | MB_ICONERROR);
+		closesocket(sfd);
+		PostQuitMessage(0);
+		WSACleanup();
+		return 0;
+	}
+	// Run the message loop.
 
-    MSG msg = {};
-    while (GetMessage(&msg, NULL, 0, 0) != 0)
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-    WSACleanup();
-    return 0;
+	MSG msg = {};
+	while (GetMessage(&msg, NULL, 0, 0) != 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	WSACleanup();
+	return 0;
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    char *messageToSend = new char[10];
-    memset(messageToSend, 0, 10);
-    switch (uMsg)
-    {
-    case WM_DESTROY:
-    {
-        closesocket(nfd);
+	char *messageToSend = new char[10];
+	memset(messageToSend, 0, 10);
+	RECT clientRect;
+	GetClientRect(hwnd, &clientRect);
+	int width = clientRect.right - clientRect.left;
+	int height = clientRect.bottom - clientRect.top;
 
-        PostQuitMessage(0);
-        return 0;
-    }
-    case WM_MOUSEMOVE:
-    {
-        messageToSend[0] = 1;
-        break;
-    }
-    case WM_LBUTTONDOWN:
-    {
-        messageToSend[0] = 2;
-        int n = 0;
-        if ((n = send(nfd, messageToSend, 5, 0)) == SOCKET_ERROR)
-        {
-            int errorCode = WSAGetLastError();
+	cout << "height" << height << " " << "width" << width << endl;
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+	{
+		closesocket(nfd);
 
-            wchar_t errorMsg[256];
-            swprintf(errorMsg, 256, L"send() error: %d", errorCode);
-            MessageBox(NULL, errorMsg, L"Error", MB_OK | MB_ICONERROR);
-        }
+		PostQuitMessage(0);
+		return 0;
+	}
+	case WM_MOUSEMOVE:
+	{
+		messageToSend[0] = 1;
+		break;
+	}
+	case WM_LBUTTONDOWN:
+	{
+		messageToSend[0] = 2;
+		break;
+	}
+	case WM_LBUTTONUP:
+	{
+		messageToSend[0] = 3;
+		break;
+	}
+	case WM_LBUTTONDBLCLK:
+	{
+		messageToSend[0] = 4;
+		break;
+	}
+	case WM_RBUTTONDOWN:
+	{
+		messageToSend[0] = 5;
+		break;
+	}
+	case WM_RBUTTONUP:
+	{
+		messageToSend[0] = 6;
+		break;
+	}
+	case WM_RBUTTONDBLCLK:
+	{
+		messageToSend[0] = 7;
 
-        break;
-    }
-    case WM_LBUTTONUP:
-    {
-        messageToSend[0] = 3;
-        break;
-    }
-    case WM_LBUTTONDBLCLK:
-    {
-        messageToSend[0] = 4;
-        break;
-    }
-    case WM_RBUTTONDOWN:
-    {
-        messageToSend[0] = 5;
-        break;
-    }
-    case WM_RBUTTONUP:
-    {
-        messageToSend[0] = 6;
+		break;
+	}
+	case WM_KEYDOWN:
+	{
+		messageToSend[0] = 8;
+		POINT pt;
 
-        break;
-    }
-    case WM_RBUTTONDBLCLK:
-    {
-        messageToSend[0] = 7;
+		ClientToScreen(hwnd, &pt);
 
-        break;
-    }
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-        EndPaint(hwnd, &ps);
-        break;
-    }
-    default:
-        return DefWindowProc(hwnd, uMsg, wParam, lParam);
-    }
-    POINT pt;
-    pt.x = LOWORD(lParam);
-    pt.y = HIWORD(lParam);
-    ClientToScreen(hwnd, &pt);
-    messageToSend[1] = ((pt.x) & (0xFF));
-    messageToSend[2] = (((pt.x) >> 8) & (0xff));
-    messageToSend[3] = ((pt.y) & (0xFF));
-    messageToSend[4] = (((pt.y) >> 8) & (0xff));
-    send(nfd, messageToSend, 10, 0);
+		messageToSend[1] = ((wParam) & (0xFF));
+		messageToSend[2] = (((wParam) >> 8) & (0xff));
+		messageToSend[3] = 0;
+		string s = to_string(static_cast<UINT16>(wParam));
+		LogMessage(s);
+		send(nfd, messageToSend, 10, 0);
+		return 0;
+	}
+	case WM_KEYUP:
+	{
+		messageToSend[0] = 8;
+		POINT pt;
 
-    return 0;
+		ClientToScreen(hwnd, &pt);
+
+		messageToSend[1] = ((wParam) & (0xFF));
+		messageToSend[2] = (((wParam) >> 8) & (0xff));
+		messageToSend[3] = 1;
+		string s = "";
+		s.push_back(static_cast<unsigned char>(static_cast<UINT16>(wParam)));
+		LogMessage(s);
+		send(nfd, messageToSend, 10, 0);
+		return 0;
+	}
+
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hwnd, &ps);
+		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+		EndPaint(hwnd, &ps);
+		break;
+	}
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	POINT pt;
+	pt.x = LOWORD(lParam);
+	pt.y = HIWORD(lParam);
+	ClientToScreen(hwnd, &pt);
+	messageToSend[1] = ((pt.x) & (0xFF));
+	messageToSend[2] = (((pt.x) >> 8) & (0xff));
+	messageToSend[3] = ((pt.y) & (0xFF));
+	messageToSend[4] = (((pt.y) >> 8) & (0xff));
+	messageToSend[5] = ((width) & (0xFF));
+	messageToSend[6] = (((width) >> 8) & (0xff));
+	messageToSend[7] = ((height) & (0xFF));
+	messageToSend[8] = ((height) >> 8) & (0xff);
+	send(nfd, messageToSend, 10, 0);
+
+	return 0;
+}
+void LogMessage(const std::string &message)
+{
+	std::ofstream logFile("log.txt", std::ios::app); // Open file in append mode
+	if (logFile.is_open())
+	{
+		logFile << message << std::endl;
+		logFile.close();
+	}
 }
